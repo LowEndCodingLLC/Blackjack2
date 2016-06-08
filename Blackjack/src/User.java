3c377dd36383a8aa1fefe5edc34608843e86a288
Stephen <smaransky@comcast.net>
2016-06-08 13:02:42 -0400

    Backend Chips Stuff

--- Blackjack/src/User.java
+++ Blackjack/src/User.java
@@ -1,13 +1,33 @@
+import javax.swing.JOptionPane;
+
 
 public class User extends Player {
-	
+	private int chipPile=100;
+	private int chipsBet;
 	public User(Dealer myDealer){
 		super();
 		super.myDealer=myDealer;
 		
 		
 	}
-
+	public void askForBet(){
+		//chipsBet=JOptionPane.showInputDialog(null, chipPile, null, chipPile);
+	}
+	public void bet(int betAmount){
+		if (betAmount>chipPile)
+			this.askForBet();
+		else{
+			chipPile-=betAmount;
+			chipsBet=betAmount;
+			this.setCanHit(true);
+		}
+		
+	}
+	public void chipMultiplier(double multiplier){
+		chipPile+=(int)(chipsBet*multiplier)/1;
+		chipsBet=0;
+	}
+	
 	@Override
 	public void endTurn() {
 		// TODO Auto-generated method stub
